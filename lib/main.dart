7ddc8e4481ef9a39a274e6956904fff7d5082082
pashangshangpo <pashangshangpo@gmail.com>
2019-01-06 12:19:12 +0800

    refactor: 拆分 home 到 pages 目录下

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:resume_app/pages/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,37 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'hello world',
-            ),
-          ],
-        ),
-      ),
+      home: HomePage(title: 'Home Page'),
     );
   }
 }
