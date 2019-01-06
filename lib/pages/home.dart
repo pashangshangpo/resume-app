3591c2d0e2c914bd87dfa7488592b138c8b95ad9
pashangshangpo <pashangshangpo@gmail.com>
2019-01-06 21:09:33 +0800

    refactor: 移动配置到 build 中

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,87 +16,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   var fileName = 'resume_v1.json';
 
-  static List _infoList = [
-    {
-      'type': 'TextField',
-      'config': {
-        'decoration': InputDecoration(
-          labelText: '姓名',
-          hintText: '请输入姓名',
-        ),
-      }
-    },
-    {
-      'type': 'TextField',
-      'config': {
-        'decoration': InputDecoration(
-          labelText: '性别',
-          hintText: '请输入性别',
-        ),
-      }
-    },
-    {
-      'type': 'TextField',
-      'config': {
-        'decoration': InputDecoration(
-          labelText: '年龄',
-          hintText: '请输入年龄',
-        ),
-      }
-    },
-    {
-      'type': 'TextField',
-      'config': {
-        'decoration': InputDecoration(
-          labelText: '学历',
-          hintText: '请输入学历',
-        ),
-      }
-    },
-    {
-      'type': 'TextField',
-      'config': {
-        'decoration': InputDecoration(
-          labelText: '工作经验',
-          hintText: '请输入工作经验',
-        ),
-      }
-    },
-    {
-      'type': 'TextField',
-      'config': {
-        'maxLines': 3,
-        'decoration': InputDecoration(
-          labelText: '自我介绍',
-          hintText: '请输入自我介绍',
-        ),
-      }
-    },
-  ];
-  static List _controllerList = [];
-
-  var inputList = _infoList.map<Widget>((info) {
-    String type = info['type'];
-    Map config = info['config'];
-    var infoText;
-    var controller = new TextEditingController();
-
-    switch (type) {
-      case 'TextField':
-        infoText = new TextField(
-          controller: controller,
-          maxLines: config['maxLines'],
-          decoration: config['decoration'],
-        );
-
-        break;
-    }
-
-    _controllerList.add(controller);
-
-    return infoText;
-  }).toList();
-
   @override
   initState() {
     super.initState();
@@ -115,17 +34,99 @@ class _HomePageState extends State<HomePage> {
     return new File('$dir/$fileName');
   }
 
-  void _saveData() async {
-    // File resumeFile = await _getFile(fileName);
-
-    // resumeFile.writeAsString(json.encode({
-    //   'name': _nameController.text,
-    //   'sex': _sexController.text,
-    // }));
-  }
-
   @override
   Widget build(BuildContext context) {
+    List _infoList = [
+      {
+        'type': 'TextField',
+        'config': {
+          'decoration': new InputDecoration(
+            labelText: '姓名',
+            hintText: '请输入姓名',
+          ),
+        }
+      },
+      {
+        'type': 'TextField',
+        'config': {
+          'decoration': new InputDecoration(
+            labelText: '性别',
+            hintText: '请输入性别',
+          ),
+        }
+      },
+      {
+        'type': 'TextField',
+        'config': {
+          'decoration': new InputDecoration(
+            labelText: '年龄',
+            hintText: '请输入年龄',
+          ),
+        }
+      },
+      {
+        'type': 'TextField',
+        'config': {
+          'decoration': new InputDecoration(
+            labelText: '学历',
+            hintText: '请输入学历',
+          ),
+        }
+      },
+      {
+        'type': 'TextField',
+        'config': {
+          'decoration': new InputDecoration(
+            labelText: '工作经验',
+            hintText: '请输入工作经验',
+          ),
+        }
+      },
+      {
+        'type': 'TextField',
+        'config': {
+          'maxLines': 3,
+          'decoration': new InputDecoration(
+            labelText: '自我介绍',
+            hintText: '请输入自我介绍',
+          ),
+        }
+      },
+    ];
+  
+    List _controllerList = [];
+
+    var inputList = _infoList.map<Widget>((info) {
+      String type = info['type'];
+      Map config = info['config'];
+      var infoText;
+      var controller = new TextEditingController();
+
+      switch (type) {
+        case 'TextField':
+          infoText = new TextField(
+            controller: controller,
+            maxLines: config['maxLines'],
+            decoration: config['decoration'],
+          );
+
+          break;
+      }
+
+      _controllerList.add(controller);
+
+      return infoText;
+    }).toList();
+
+    void _saveData() async {
+      // File resumeFile = await _getFile(fileName);
+
+      // resumeFile.writeAsString(json.encode({
+      //   'name': _nameController.text,
+      //   'sex': _sexController.text,
+      // }));
+    }
+
     return Scaffold(
       body: Container(
         margin: const EdgeInsets.all(12.0),
