6860523fbf3d29906fbf4093c35b6ba45a378862
pashangshangpo <pashangshangpo@gmail.com>
2019-01-06 20:01:00 +0800

    refactor: 表单元素通过遍历 List 添加

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -25,10 +25,12 @@ class _HomePageState extends State<HomePage> {
   List infoList = [
     {
       'type': 'TextField',
-      'decoration': InputDecoration(
-        labelText: '姓名',
-        hintText: '请输入姓名',
-      ),
+      'config': {
+        'decoration': InputDecoration(
+          labelText: '姓名',
+          hintText: '请输入姓名',
+        ),
+      }
     }
   ];
 
@@ -64,7 +66,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    var inputList = infoList.map((info) {
+    var inputList = infoList.map<Widget>((info) {
       String type = info['type'];
       var infoText;
 
@@ -72,7 +74,7 @@ class _HomePageState extends State<HomePage> {
         case 'TextField':
           infoText = new TextField(
             controller: new TextEditingController(),
-            decoration: info['decoration'],
+            decoration: info['config']['decoration'],
           );
 
           break;
@@ -81,57 +83,11 @@ class _HomePageState extends State<HomePage> {
       return infoText;
     }).toList();
 
-    print(inputList[0].controller);
-
     return Scaffold(
       body: Container(
         margin: const EdgeInsets.all(12.0),
         child: Column(
-          children: <Widget>[
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(
-                labelText: '姓名',
-                hintText: '请输入姓名',
-              ),
-            ),
-            TextField(
-              controller: _sexController,
-              decoration: InputDecoration(
-                labelText: '性别',
-                hintText: '请输入性别',
-              ),
-            ),
-            TextField(
-              controller: _ageController,
-              decoration: InputDecoration(
-                labelText: '年龄',
-                hintText: '请输入年龄',
-              ),
-            ),
-            TextField(
-              controller: _educationController,
-              decoration: InputDecoration(
-                labelText: '学历',
-                hintText: '请输入学历',
-              ),
-            ),
-            TextField(
-              controller: _fixedNumberYearController,
-              decoration: InputDecoration(
-                labelText: '工作经验',
-                hintText: '请输入工作经验',
-              ),
-            ),
-            TextField(
-              controller: _introduceController,
-              maxLines: 3,
-              decoration: InputDecoration(
-                labelText: '自我介绍',
-                hintText: '请输入自我介绍',
-              ),
-            ),
-          ],
+          children: inputList,
         ),
       ),
       floatingActionButton: new FloatingActionButton(
