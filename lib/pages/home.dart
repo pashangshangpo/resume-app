04cbbf4fd5e2c67e2c65c0cf06d464e4a560cdbc
pashangshangpo <pashangshangpo@gmail.com>
2019-01-06 15:19:08 +0800

    style: 设置间距

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -20,53 +20,56 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          TextField(
-            controller: _nameController,
-            decoration: InputDecoration(
-              labelText: '姓名',
-              hintText: '请输入姓名',
+      body: Container(
+        margin: const EdgeInsets.all(12.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: '姓名',
+                hintText: '请输入姓名',
+              ),
             ),
-          ),
-          TextField(
-            controller: _sexController,
-            decoration: InputDecoration(
-              labelText: '性别',
-              hintText: '请输入性别',
+            TextField(
+              controller: _sexController,
+              decoration: InputDecoration(
+                labelText: '性别',
+                hintText: '请输入性别',
+              ),
             ),
-          ),
-          TextField(
-            controller: _ageController,
-            decoration: InputDecoration(
-              labelText: '年龄',
-              hintText: '请输入年龄',
+            TextField(
+              controller: _ageController,
+              decoration: InputDecoration(
+                labelText: '年龄',
+                hintText: '请输入年龄',
+              ),
             ),
-          ),
-          TextField(
-            controller: _educationController,
-            decoration: InputDecoration(
-              labelText: '学历',
-              hintText: '请输入学历',
+            TextField(
+              controller: _educationController,
+              decoration: InputDecoration(
+                labelText: '学历',
+                hintText: '请输入学历',
+              ),
             ),
-          ),
-          TextField(
-            controller: _fixedNumberYearController,
-            decoration: InputDecoration(
-              labelText: '工作经验',
-              hintText: '请输入工作经验',
+            TextField(
+              controller: _fixedNumberYearController,
+              decoration: InputDecoration(
+                labelText: '工作经验',
+                hintText: '请输入工作经验',
+              ),
             ),
-          ),
-          TextField(
-            controller: _introduceController,
-            maxLines: 3,
-            decoration: InputDecoration(
-              labelText: '自我介绍',
-              hintText: '请输入自我介绍',
+            TextField(
+              controller: _introduceController,
+              maxLines: 3,
+              decoration: InputDecoration(
+                labelText: '自我介绍',
+                hintText: '请输入自我介绍',
+              ),
             ),
-          ),
-        ],
-      ),
+          ],
+        ),
+      )
     );
   }
 }
